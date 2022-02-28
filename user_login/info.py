bf41658b3e13e97071a8ff9a79e5facb703443c9
Chianjit Rathod <chiranjitrathod1@mail.com>
2022-02-28 11:40:57 +0530

    updated css

--- user_login/info.py
+++ user_login/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = '#enter host email here '
-EMAIL_HOST_PASSWORD = 'password'
+EMAIL_HOST_USER = 'chiranjitrathod.work@gmail.com'
+EMAIL_HOST_PASSWORD = 'RaThOd@7781'
 EMAIL_PORT = 587
